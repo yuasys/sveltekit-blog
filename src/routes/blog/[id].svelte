f995839b4458ac30f0ca295df42b4567eb5b20d2
湯浅 信彦 <2534721+yuasys@users.noreply.github.com>
2021-08-27 09:44:41 +0900

    sveltekit tutorial 11

--- src/routes/blog/[id].svelte
+++ src/routes/blog/[id].svelte
@@ -1,14 +1,17 @@
 <script context="module">
 import { page } from "$app/stores";
 
-
   export const load = async ({ page, fetch }) => {
     const id = page.params.id
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     const post = await res.json()
+
+    const userRes = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+    const user = await userRes.json()
     return {
       props: {
         post,
+        user,
       }
     }
   }
@@ -16,7 +19,9 @@ import { page } from "$app/stores";
 
 <script>
   export let post
+  export let user
 </script>
 
 <h1>{post.title}</h1>
 <p>{post.body}</p>
+<p>- Written by <a href={`/authors/${user.id}`}>{user.name}</a></p>
